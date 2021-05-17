a9ba72722d7139b8076d88a45d27dc492a169155
7774j <80786885+7774j@users.noreply.github.com>
2021-05-17 11:05:41 -0500

    This is me trying to use GitHub
    I have a back-up at least

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles: [`
+    //h3 {
+      //color: dodgerblue;
 })
+
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Jana';
+  username = '';
+  showSecret = false;
+  log = [];
+  //numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
+  value = 5;
+
+onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length + 1);
+    //this.log.push(new Date());
+    //I tried to use this.log.push(new Date()); but there was an error of parameter set to never.
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './alert-assignment/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './alert-assignment/success-alert/success-alert.component';
+import { BasicHighlightDirective } from './directive-deep-dive-assignment/basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './directive-deep-dive-assignment/better-highlight/better-highlight.directive';
+import { UnlessDirective } from './directive-deep-dive-assignment/unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
